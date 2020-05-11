3e3fe0e596baab06689c33e20186c38beac6b34a
Rohit1204 <rohitahuja725@gmail.com>
2020-05-11 15:32:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TodoItems from './TodoItems';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      items:[],
+    };
+    this.addItem=this.addItem.bind(this);
+    this.deleteItem=this.deleteItem.bind(this);
+  }
+  addItem(e){
+    if(this._inputElement.value !==" "){
+      var newItem ={
+        text: this._inputElement.value,
+        key:Date.now()
+      };
+      this.setState((prevState)=>{
+        return{
+          items:prevState.items.concat(newItem)
+        };
+      });
+      this._inputElement.value=" ";
+    }
+  console.log(this.state.items);
+  e.preventDefault();
+  }
+  deleteItem(key){
+    var filteredItems= this.state.items.filter(function(item){
+      return (item.key !==key);
+    });
+    this.setState({
+      items:filteredItems
+    });
+  }
+render()
+{
+  return(
+    <div className="todoListMain">
+      <h1 className="title">To-Do List </h1>
+      <div className="header">
+        <form onSubmit ={ this.addItem}>
+      <input 
+      ref={(a) => this._inputElement =a}
+      placeholder="enter task">
+      </input>
+      <button type="submit">add</button>
+        </form>
+      </div>
+      <TodoItems entries= {this.state.items}
+                  delete={this.deleteItem}
+                  />
     </div>
   );
+}  
 }
 
 export default App;
---
+++ src/TodoItems.js
@@ -0,0 +1,37 @@
+import React, { Component ,Fragment} from 'react';
+import FlipMove from 'react-flip-move';
+import './TodoItems.css';
+class TodoItems extends Component{
+  constructor(props)
+  {
+    super(props);
+    this.createTasks=this.createTasks.bind(this);
+  }
+  delete(key){
+    this.props.delete(key);
+     }
+  createTasks(item){
+    return (
+          <li key={item.key}>{item.text}
+          <button type="button" onClick={()=> this.delete(item.key)}>X</button>
+    </li>
+    
+   
+    );
+  }
+ 
+  render(){
+    var todoEntries =this.props.entries;
+    var listItems=todoEntries.map(this.createTasks);
+    return(
+      <ul className="theList">
+        <FlipMove duration={250} easing="ease-out">
+        {listItems}
+        </FlipMove>
+      </ul>
+    );
+  }
+};
+
+
+export default TodoItems;
